b865c0166ceb5609557cf118ad593497c417b535
pavithra1569 <pavithrak.23it@kongu.edu>
2025-06-22 13:57:43 +0530

    image insert

--- react/src/page/Home.jsx
+++ react/src/page/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import About from './About'
+import image from '../assets/background.jpeg'
 const Home = ({items,users}) => {
   return (
     <>
     <div>
      <About items={items} users={users}/>
+     <img src={image} alt="react"></img>
     </div>
     </>
   )
